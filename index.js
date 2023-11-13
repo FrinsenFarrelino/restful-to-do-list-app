bb3ae45505abf4fa0ea296174e77993d17f3576b
FrinsenFarrelino <frinsenfarrelino@gmail.com>
2023-11-13 21:07:12 +0700

    connect to database sequelize

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const authRoutes = require("./routes/authRoutes");
 const todoRoutes = require("./routes/todoRoutes");
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, { logging: false });
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    dialectModule: require("mysql2"),
+  }
+);
 const User = require("./models/Users")(sequelize);
 const Todo = require("./models/Todos")(sequelize);
 
